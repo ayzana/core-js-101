e29ef284ba4a0fb977a90810f2ba6da77ce70ecd
ayzana <aizanka001@gmail.com>
2023-10-18 16:25:57 +0300

    feat: add methods

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -1,169 +1,36 @@
-function concatenateStrings(value1, value2) {
- return (value1 + value2);
-}
+function concatenateStrings(value1, value2) { return (value1 + value2); }
+function getStringLength(value) { return value.length; }
 
-function getStringLength(value) {
-  return value.length;
-}
+function getStringFromTemplate(firstName, lastName) { return `Hello, ${firstName} ${lastName}!`; }
 
-/* Returns the result of string template and given parameters firstName and lastName.
- * Please do not use concatenation, use template string :
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
- *
- * @param {string} firstName
- * @param {string} lastName
- * @return {string}
- *
- * @example
- *   'John','Doe'      => 'Hello, John Doe!'
- *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
- */
-function getStringFromTemplate(firstName, lastName) {
-  return `Hello, ${firstName} ${lastName}!`;
-}
-
-/* Extracts a name from template string 'Hello, First_Name Last_Name!'.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
- *   'Hello, John Doe!' => 'John Doe'
- *   'Hello, Chuck Norris!' => 'Chuck Norris'
- */
 function extractNameFromTemplate(value) {
   return value.substring(7, value.length - 1);
 }
 
-
-/* Returns a first char of the given string.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
- *   'John Doe'  => 'J'
- *   'cat'       => 'c'
- */
 function getFirstChar(value) {
  return value[0];
 }
 
-/* Removes a leading and trailing whitespace characters from string.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
- *   '  Abracadabra'    => 'Abracadabra'
- *   'cat'              => 'cat'
- *   '\tHello, World! ' => 'Hello, World!'
- */
-function removeLeadingAndTrailingWhitespaces(value) {
-return value.trim();
-}
+function removeLeadingAndTrailingWhitespaces(value) { return value.trim(); }
 
-/* Returns a string that repeated the specified number of times.
- *
- * @param {string} value
- * @param {string} count
- * @return {string}
- *
- * @example
- *   'A', 5  => 'AAAAA'
- *   'cat', 3 => 'catcatcat'
- */
-function repeatString(value, count) {
-  return value.repeat(count);
-}
+function repeatString(value, count) { return value.repeat(count); }
 
-/* Remove the first occurrence of string inside another string
- *
- * @param {string} str
- * @param {string} value
- * @return {string}
- *
- * @example
- *   'To be or not to be', 'not'  => 'To be or  to be'
- *   'I like legends', 'end' => 'I like legs',
- *   'ABABAB','BA' => 'ABAB'
- */
 function removeFirstOccurrences(str, value) {
   return str.replace(value, '');
 }
 
-/**
- * Remove the first and last angle brackets from tag string
- *
- * @param {string} str
- * @return {string}
- *
- * @example
- *   '<div>' => 'div'
- *   '<span>' => 'span'
- *   '<a>' => 'a'
- */
 function unbracketTag(str) {
   return (str.replaceAll('<', '')).replaceAll('>', '');
 }
 
-
-/**
- * Converts all characters of the specified string into the upper case
- *
- * @param {string} str
- * @return {string}
- *
- * @example
- *   'Thunderstruck' => 'THUNDERSTRUCK'
- *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
- */
 function convertToUpperCase(str) {
   return str.toUpperCase();
 }
 
-/**
- * Extracts e-mails from single string with e-mails list delimeted by semicolons
- *
- * @param {string} str
- * @return {array}
- *
- * @example
- *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com'
- *   => [
- *      'angus.young@gmail.com',
- *      'brian.johnson@hotmail.com',
- *      'bon.scott@yahoo.com'
- *   ],
- *   'info@gmail.com' => ['info@gmail.com']
- */
 function extractEmails(str) {
 return str.split(';');
 }
 
-/**
- * Returns the string representation of rectangle with specified width and height
- * using pseudograhic chars
- *
- * @param {number} width
- * @param {number} height
- * @return {string}
- *
- * @example
- *
- *            '┌────┐\n'+
- *  (6,4) =>  '│    │\n'+
- *            '│    │\n'+
- *            '└────┘\n'
- *
- *  (2,2) =>  '┌┐\n'+
- *            '└┘\n'
- *
- *             '┌──────────┐\n'+
- *  (12,3) =>  '│          │\n'+
- *             '└──────────┘\n'
- *
- */
 function getRectangleString(width, height) {
   return `┌${'─'.repeat(width - 2)}┐\n${(`│${(' ').repeat(width - 2)}│\n`).repeat(height - 2)}└${'─'.repeat(width - 2)}┘\n`;
 }
@@ -233,7 +100,6 @@ function getCardId(/* value */) {
   throw new Error('Not implemented');
 }
 
-
 module.exports = {
   concatenateStrings,
   getStringLength,
@@ -247,7 +113,7 @@ module.exports = {
   convertToUpperCase,
   extractEmails,
   getRectangleString,
-encodeToRot13,
-isString,
-getCardId,
+  encodeToRot13,
+  isString,
+  getCardId,
 };
